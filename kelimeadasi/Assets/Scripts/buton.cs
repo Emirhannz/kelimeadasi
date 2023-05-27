e1a1b1a9322ed9bb3d5262a09f1e5ce3657299c4
Emirhan Zileli <emirhanzileli@gmail.com>
2023-05-27 16:47:44 +0300

    Revert "16.43 cmt"
    This reverts commit 592b242aeecc9b58045de1e572c1403efb4b5322.

--- kelimeadasi/Assets/Scripts/buton.cs
+++ kelimeadasi/Assets/Scripts/buton.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class buton : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     yonetici yonet;
 
