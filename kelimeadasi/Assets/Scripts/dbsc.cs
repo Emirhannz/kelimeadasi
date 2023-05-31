8b6a1d1f56e43731591d75bfd73da1a34efe7417
Alperen Sarıkavak <alperensarikavak2@gmail.com>
2023-05-31 15:41:22 +0300

    DB-SCENE eklendi
    SCENE BİTMEDİ

---
+++ kelimeadasi/Assets/Scripts/dbsc.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mono.Data.Sqlite;
+using JetBrains.Annotations;
+using UnityEngine.UI;
+
+public class dbsc : MonoBehaviour
+{
+    public InputField nickName;
+    public InputField password;
+
+
+    private string dbName = "URI=file:loginscreen.log";
+    void Start()
+    {  
+        
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
