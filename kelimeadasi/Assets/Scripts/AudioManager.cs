2a7af4006af83b74f032f7443c79c8200df051a5
Emirhan Zileli <emirhanzileli@gmail.com>
2023-06-03 16:53:48 +0300

    butonsesefekti

---
+++ kelimeadasi/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager instance;
+
+    public AudioSource menuMusic; // Anamenü müziðini temsil eden AudioSource
+    public AudioSource gameMusic; // Oyun ekraný müziðini temsil eden AudioSource
+    public AudioSource saat;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        // Anamenü müziðini baþlat
+        menuMusic.Play();
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "anamennnu")
+        {
+            if (!menuMusic.isPlaying)
+            {
+                menuMusic.Play();
+            }
+            saat.Stop();
+            gameMusic.Stop();
+        }
+        else if (scene.name == "GameScreen")
+        {
+            menuMusic.Stop();
+
+            if (!gameMusic.isPlaying)
+            {
+                gameMusic.Play();
+                saat.Play();
+            }
+        }
+        else if (scene.name == "AYARLAR" || scene.name == "SkorTablosu")
+        {
+            if (!menuMusic.isPlaying)
+            {
+                menuMusic.UnPause();
+            }
+
+            gameMusic.Stop();
+            saat.Stop();
+        }
+    }
+
+}
